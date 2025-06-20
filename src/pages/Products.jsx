960efaa6508a3c94149654d739373d39eb45fcab
vynas <hamdabousnina@gmail.com>
2025-06-20 11:45:58 -0400

    filters - params

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,10 +4,13 @@ import { customFetch } from "../utils";
 const url = "products/";
 
 export const loader = async ({ request }) => {
-  const res = await customFetch(url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const res = await customFetch(url, { params });
   const products = res.data.data;
   const meta = res.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => {
