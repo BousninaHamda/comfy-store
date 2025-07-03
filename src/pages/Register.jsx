e6e7e46f2aaefce4d8d85da0e2634adfc33b8945
vynas <hamdabousnina@gmail.com>
2025-07-03 10:37:07 -0400

    access RTK store in action

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,15 +8,15 @@ export const action = async ({ request }) => {
   const data = Object.fromEntries(formData);
   try {
     const response = await customFetch.post("/auth/local/register", data);
-    toast.success("accont created successfully");
+    toast.success("account created successfully");
     return redirect("/login");
   } catch (error) {
     const errorMessage =
       error?.response?.data?.error?.message ||
       "please double check your credentials";
     toast.error(errorMessage);
+    return null;
   }
-  return null;
 };
 
 const Register = () => {
